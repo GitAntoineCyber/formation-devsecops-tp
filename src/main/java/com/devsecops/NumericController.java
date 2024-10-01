084c33f948fd35164dd5306fbca6ad880d0a4742
GitAntoineCyber <antoine.cyber01@gmail.com>
2024-10-01 16:34:57 +0200

    Update NumericController.java

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -27,7 +27,7 @@ public class NumericController {
 
 		@GetMapping("/")
 		public String welcome() {
-			return "Kubernetes DevSecOps";
+			return "Kubernetes DevSecjhjhghjOps";
 		}
 
 		@GetMapping("/compare/{value}")
